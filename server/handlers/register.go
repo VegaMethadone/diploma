4ac711901e9e35dc9fe97cd34cc6af46d2575f00
vega <vegamethadone@yandex.ru>
2025-03-18 01:42:47 +0300

    added some  handlers and tamplate of register handler

---
+++ server/handlers/register.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type RegisterRequest struct {
+	Email    string `json: "email"`
+	Password string `json" "password"`
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req RegisterRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "Email and password are required", http.StatusBadRequest)
+		return
+	}
+
+	/*
+		логика для регистрации юзера в бд
+	*/
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "User registered successfully",
+	})
+
+}
