88618258ff5c6e001b245f5f7dd470d1e24af1d8
vega <vegamethadone@yandex.ru>
2025-04-24 23:47:59 +0300

    fixed delte  bucket and test

--- database/minio/bucket/delete_bucket.go
+++ database/minio/bucket/delete_bucket.go
@@ -3,7 +3,6 @@ package bucket
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/minio/minio-go/v7"
 )
@@ -12,24 +11,19 @@ func (b *BucketMINIO) DeleteForce(
 	ctx context.Context,
 	bucketName string,
 ) error {
-	err := b.Client.RemoveBucket(ctx, bucketName)
-	if err == nil {
-		return nil
-	}
-
-	if !strings.Contains(err.Error(), "bucket not empty") {
-		return fmt.Errorf("initial bucket removal failed: %w", err)
-	}
-
 	objectsCh := b.Client.ListObjects(ctx, bucketName, minio.ListObjectsOptions{
 		Recursive: true,
 	})
 
-	for rErr := range b.Client.RemoveObjects(ctx, bucketName, objectsCh, minio.RemoveObjectsOptions{}) {
-		if rErr.Err != nil {
-			return fmt.Errorf("failed to remove object: %w", rErr.Err)
+	for err := range b.Client.RemoveObjects(ctx, bucketName, objectsCh, minio.RemoveObjectsOptions{}) {
+		if err.Err != nil {
+			return fmt.Errorf("failed to remove object %s: %w", err.ObjectName, err.Err)
 		}
 	}
 
-	return b.Client.RemoveBucket(ctx, bucketName)
+	if err := b.Client.RemoveBucket(ctx, bucketName); err != nil {
+		return fmt.Errorf("failed to remove bucket: %w", err)
+	}
+
+	return nil
 }
